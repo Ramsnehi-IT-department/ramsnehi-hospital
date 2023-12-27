3e69aa8567a21ea0765c01c15189593568564b98
Pradeep Vijayvargiya <ramsnehisoftwaredept@gmail.com>
2023-12-27 18:25:31 +0530

    Work on navbar

--- resources/views/frontends/partials/header.blade.php
+++ resources/views/frontends/partials/header.blade.php
@@ -27,7 +27,7 @@
 
     {{-- header nav bar start --}}
     <nav class="navbar navbar-expand-lg navigation" id="navbar">
-        <div class="container">
+        <div class="container-fluid">
             <a class="navbar-brand" href="/">
                 <img src="images/logo.png" alt="Ramsnehi-Logo" class="img-fluid">
             </a>
@@ -60,85 +60,95 @@
                                     href="/president">{{ __('homepage.AboutUs_president') }}</a></li>
                             <li><a class="dropdown-item"
                                     href="/secretary">{{ __('homepage.AboutUs_secretory') }}</a></li>
+                            <li><a class="dropdown-item" href="/">Mangement Team</a></li>
                             <li><a class="dropdown-item"
                                     href="/certificate">{{ __('homepage.AboutUs_certificates') }}</a>
                             </li>
-
-                            {{-- <li class="dropdown dropdown-submenu dropright">
-                                        <a class="dropdown-item dropdown-toggle" href="#!" id="dropdown0301" role="button"
-                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sub Menu</a>
-        
-                                        <ul class="dropdown-menu" aria-labelledby="dropdown0301">
-                                            <li><a class="dropdown-item" href="index.html">Submenu 01</a></li>
-                                            <li><a class="dropdown-item" href="index.html">Submenu 02</a></li>
-                                        </ul>
-                                    </li> --}}
                         </ul>
                     </li>
                     {{-- AboutUs end --}}
 
-                    {{-- Care Center Start --}}
+                    {{-- Hospital Start --}}
                     <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown02" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">Hospital <i class="icofont-thin-down"></i></a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdown02">
+                            <li><a class="dropdown-item" href="/">OPD Timing</a></li>
+                            <li><a class="dropdown-item" href="/">Health Check up plan</a>
+                            </li>
+                            <li><a class="dropdown-item" href="/">Government Schemes</a></li>
+                            <li><a class="dropdown-item" href="/">TPA Empanelment</a></li>
+                            <li><a class="dropdown-item" href="/">Patient Testimonial</a></li>
+                            <li><a class="dropdown-item" href="/">Patient success stories</a>
+                            </li>
+                            <li><a class="dropdown-item" href="/">Photo Gallery</a>
+                            </li>
+                        </ul>
+                    </li>
+                    {{-- Hospital end --}}
+
+                    {{-- Care Center Start --}}
+                    {{-- <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="dropdown02" data-toggle="dropdown"
                             aria-haspopup="true"
                             aria-expanded="false">{{ __('homepage.careCenter_menu') }} <i
-                                class="icofont-thin-down"></i></a>
-                        <ul class="dropdown-menu" aria-labelledby="dropdown02">
-                            <div class="row">
-                                <div class="col-lg-6 col-md-12">
-                                    <li><a class="dropdown-item"
-                                            href="#">{{ __('homepage.careCenter_menu') }}</a></li>
-                                    <li><a class="dropdown-item"
-                                            href="#">{{ __('homepage.careCenter_menu') }}</a></li>
-                                    <li><a class="dropdown-item"
-                                            href="#">{{ __('homepage.careCenter_menu') }}</a></li>
-                                </div>
-                                <div class="col-lg-6 col-md-12">
-                                    <li><a class="dropdown-item"
-                                            href="#">{{ __('homepage.careCenter_menu') }}</a></li>
-                                    <li><a class="dropdown-item"
-                                            href="#">{{ __('homepage.careCenter_menu') }}</a></li>
-                                    <li><a class="dropdown-item"
-                                            href="#">{{ __('homepage.careCenter_menu') }}</a></li>
-                                </div>
+                        class="icofont-thin-down"></i></a>
+                    <ul class="dropdown-menu" aria-labelledby="dropdown02">
+                        <div class="row">
+                            <div class="col-lg-6 col-md-12">
+                                <li><a class="dropdown-item"
+                                        href="#">{{ __('homepage.careCenter_menu') }}</a></li>
+                                <li><a class="dropdown-item"
+                                        href="#">{{ __('homepage.careCenter_menu') }}</a></li>
+                                <li><a class="dropdown-item"
+                                        href="#">{{ __('homepage.careCenter_menu') }}</a></li>
                             </div>
-                        </ul>
-                    </li>
+                            <div class="col-lg-6 col-md-12">
+                                <li><a class="dropdown-item"
+                                        href="#">{{ __('homepage.careCenter_menu') }}</a></li>
+                                <li><a class="dropdown-item"
+                                        href="#">{{ __('homepage.careCenter_menu') }}</a></li>
+                                <li><a class="dropdown-item"
+                                        href="#">{{ __('homepage.careCenter_menu') }}</a></li>
+                            </div>
+                        </div>
+                    </ul>
+                    </li> --}}
                     {{-- Care Center end --}}
 
                     {{-- Visitor Services Start --}}
-                    <li class="nav-item dropdown">
+                    {{-- <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="dropdown02" data-toggle="dropdown"
                             aria-haspopup="true"
                             aria-expanded="false">{{ __('homepage.visitorServices_menu') }} <i
-                                class="icofont-thin-down"></i></a>
-                        <ul class="dropdown-menu" aria-labelledby="dropdown02">
-                            <div class="row">
-                                <div class="col-lg-6 col-md-12">
-                                    <li><a class="dropdown-item"
-                                            href="#">{{ __('homepage.visitorServices_menu') }}</a>
-                                    </li>
-                                    <li><a class="dropdown-item"
-                                            href="#">{{ __('homepage.visitorServices_menu') }}</a>
-                                    </li>
-                                    <li><a class="dropdown-item"
-                                            href="#">{{ __('homepage.visitorServices_menu') }}</a>
-                                    </li>
-                                </div>
-                                <div class="col-lg-6 col-md-12">
-                                    <li><a class="dropdown-item"
-                                            href="#">{{ __('homepage.visitorServices_menu') }}</a>
-                                    </li>
-                                    <li><a class="dropdown-item"
-                                            href="#">{{ __('homepage.visitorServices_menu') }}</a>
-                                    </li>
-                                    <li><a class="dropdown-item"
-                                            href="#">{{ __('homepage.visitorServices_menu') }}</a>
-                                    </li>
-                                </div>
+                        class="icofont-thin-down"></i></a>
+                    <ul class="dropdown-menu" aria-labelledby="dropdown02">
+                        <div class="row">
+                            <div class="col-lg-6 col-md-12">
+                                <li><a class="dropdown-item"
+                                        href="#">{{ __('homepage.visitorServices_menu') }}</a>
+                                </li>
+                                <li><a class="dropdown-item"
+                                        href="#">{{ __('homepage.visitorServices_menu') }}</a>
+                                </li>
+                                <li><a class="dropdown-item"
+                                        href="#">{{ __('homepage.visitorServices_menu') }}</a>
+                                </li>
                             </div>
-                        </ul>
-                    </li>
+                            <div class="col-lg-6 col-md-12">
+                                <li><a class="dropdown-item"
+                                        href="#">{{ __('homepage.visitorServices_menu') }}</a>
+                                </li>
+                                <li><a class="dropdown-item"
+                                        href="#">{{ __('homepage.visitorServices_menu') }}</a>
+                                </li>
+                                <li><a class="dropdown-item"
+                                        href="#">{{ __('homepage.visitorServices_menu') }}</a>
+                                </li>
+                            </div>
+                        </div>
+                    </ul>
+                    </li> --}}
                     {{-- Visitor Services end --}}
 
                     {{-- Proffessional Start --}}
@@ -156,6 +166,47 @@
                     </li>
                     {{-- Proffessional end --}}
 
+                    {{-- Events Start --}}
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown02" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">Events <i class="icofont-thin-down"></i></a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdown02">
+                            <li><a class="dropdown-item" href="#">Hospital Events</a></li>
+                            <li><a class="dropdown-item" href="#">Health Camp</a></li>
+                            <li><a class="dropdown-item" href="#">Health Talks</a></li>
+                        </ul>
+                    </li>
+                    {{-- Events end --}}
+
+                    {{-- Health Information Start --}}
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown02" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">Health Information <i
+                                class="icofont-thin-down"></i></a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdown02">
+                            <li><a class="dropdown-item" href="#">Medi Update</a></li>
+                            <li><a class="dropdown-item" href="#">Health Tips</a></li>
+                            <li><a class="dropdown-item" href="#">Blogs</a></li>
+                        </ul>
+                    </li>
+                    {{-- Health Information end --}}
+
+                    {{-- Media Coverage Start --}}
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown02" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">Media Coverage <i
+                                class="icofont-thin-down"></i></a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdown02">
+                            <li><a class="dropdown-item" href="#">Print Media</a></li>
+                            <li><a class="dropdown-item" href="#">Press Release</a></li>
+                        </ul>
+                    </li>
+                    {{-- Media Coverage end --}}
+
+                    {{-- Career with Us start --}}
+                    <li class="nav-item"><a class="nav-link" href="/">Career</a></li>
+                    {{-- Career with Us end --}}
+
                     {{-- Contact start --}}
                     <li class="nav-item"><a class="nav-link"
                             href="/contact">{{ __('homepage.contact_menu') }}</a></li>
@@ -164,7 +215,7 @@
                     {{-- Language start --}}
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle current"
-                            href="{{ url()->current() . '?_locale=en' }}">English <i
+                            href="{{ url()->current() . '?_locale=en' }}">Eng <i
                                 class="icon icofont-simple-down"></i></a>
                         <ul class="dropdown-menu" aria-labelledby="dropdown02">
                             <li><a class="dropdown-item"
